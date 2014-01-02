699ebdc6509d27f377cb9d4a7e97f1f077e8864f
jarekmisz <jarek@us.ibm.com>
2014-01-02 14:17:57 -0600

    Adding init-file attribute

--- cookbooks/sde-ovs-setup/attributes/default.rb
+++ cookbooks/sde-ovs-setup/attributes/default.rb
@@ -1,4 +1,5 @@
 default[:openvswitch][:conf_dir] = '/etc/openvswitch'
 default[:openvswitch][:pid_dir] = '/var/run/openvswitch'
+default[:openvswitch][:init_file] = '/etc/sysconfig/openvswitch'
 default[:openvswitch][:rpm_url] = 'http://sde-xcat-pre.pok.ibm.com/repos/rpms/openvswitch/openvswitch-1.10.0-4.ibm.el6.x86_64.rpm'
 default[:openvswitch][:eth_name] = 'eth2'
