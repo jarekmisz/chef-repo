90324d0d172ec8bfa826173d9bd76fb4a97efac5
jarekmisz <jarek@us.ibm.com>
2014-01-03 11:48:01 -0600

    fixed the command to retrieve bridge name adding dir

--- cookbooks/sde-ovs-setup/templates/default/interfaces-setup.sh
+++ cookbooks/sde-ovs-setup/templates/default/interfaces-setup.sh
@@ -18,7 +18,7 @@ if [ ! -d "$DIR/backup" ]; then
 fi
 
 # Check whether bridge over this pysical interface already exists
-export brdg=`cat ifcfg-$intfc | grep 'BRIDGE' | cut -d= -f2 | awk '{print $1}'`
+export brdg=`cat $DIR/ifcfg-$intfc | grep 'BRIDGE' | cut -d= -f2 | awk '{print $1}'`
 
 echo "Bridge name retrieved from the $intfc configuration file is $brdg"
 
