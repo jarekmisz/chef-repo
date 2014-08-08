f29db13ed2ecd28eb322feb680fcda8c0eed112b
jarekmisz <jarek@us.ibm.com>
2014-08-08 19:27:13 +0200

    Changed version to 0.1.2

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
-node_name                'jarek'
-client_key               'c:/Users/IBM_ADMIN/chef-repo/.chef/jarek.pem'
+node_name                'jarek-jb'
+client_key               '/home/jarek/chef-repo/.chef/jarek-jb.pem'
 validation_client_name   'chef-validator'
-validation_key           'c:/Users/IBM_ADMIN/chef-repo/.chef/chef-validator.pem'
-chef_server_url          'https://9.47.224.239'
-syntax_check_cache_path  'c:/Users/IBM_ADMIN/chef-repo/.chef/syntax_check_cache'
+validation_key           '/home/jarek/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://sde-xcat-pre.pok.ibm.com:443'
+syntax_check_cache_path  '/home/jarek/chef-repo/.chef/syntax_check_cache'
 cookbook_path [ './cookbooks' ]
